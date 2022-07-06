d8dbedf1b9205841f7519c80274f4f0c5e8f0338
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2022-07-06 08:16:33 +0000

    셰이더 조명 코드 추가

--- GLFramework/GLFramework/Shader/Fragment.glsl
+++ GLFramework/GLFramework/Shader/Fragment.glsl
@@ -2,9 +2,51 @@
 
 layout(location = 0) out vec4 color;
 
-in vec4 out_color;
+in vec3 f_position;
+in vec3 f_normal;
+in vec4 f_color;
+
+// Light
+uniform vec3 ambient_light;
+uniform vec3 light_position;
+uniform vec3 light_color;
+uniform int shininess;
+uniform vec3 view_position;
+
+vec3 AmbientLight()
+{
+	return ambient_light * light_color;
+}
+
+vec3 DiffuseLight(vec3 normal_vector, vec3 light_direction)
+{
+	float diffuse_light = max(dot(normal_vector, light_direction), 0.0);
+	
+	return diffuse_light * light_color;
+}
+
+vec3 SpecularLight(vec3 normal_vector, vec3 light_direction)
+{
+	vec3 view_direction = normallize(view_position - f_position);
+	vec3 reflect_direction = reflect(-light_direction, normal_vector);
+
+	float specular_light = max(dot(view_direction, reflect_direction), 0.0);
+	specular_light = pow(specular_light, shininess);
+
+	return specular_light * light_color;
+}
 
 void main()
 {
-	color = vec4(out_color);
+	vec3 normal_vector = normalize(f_normal);
+	vec3 light_direction = normalize(light_position - f_position);
+
+	// ambient light(주변 조명)
+	//color = vec4((AmbientLight() * f_color), 1.0);
+
+	// ambient + diffuse light(주변 조명 + 산란 반사 조명)
+	//color = vec4((AmbientLight() + DiffuseLight(normal_vector, light_direction)) * f_color, 1.0);
+
+	// ambient + diffuse + specular light(주변 조명 + 산란 반사 조명 + 거울 반사 조명)
+	color = vec4((AmbientLight() + DiffuseLight(normal_vector, light_direction) + SpecularLight(normal_vector, light_direction)) * f_color, 1.0);
 }
--- GLFramework/GLFramework/Shader/Vertex.glsl
+++ GLFramework/GLFramework/Shader/Vertex.glsl
@@ -1,17 +1,29 @@
 #version 450
 
-in vec3 position;
+// ambient, diffuse, specular 전부 적용
 
-in vec4 in_color;
-out vec4 out_color;
+// vectex shader
+in vec3 v_position;
+in vec3 v_normal;
+in vec4 v_color;
 
+// fragment shader
+out vec3 f_position;
+out vec3 f_normal;
+out vec4 f_color;
+
+// Transform Matrix
 uniform mat4 transform;
 uniform mat4 view;
 uniform mat4 projection;
 
 void main()
 {
-	gl_Position = /*projection * view */ transform * vec4(position, 1.0);
+	vec3 world = transform * vec4(v_position, 1.0);
 
-	out_color = in_color;
+	gl_Position = /*projection * view */ world;
+
+	f_position = world;
+	f_normal = v_normal;
+	f_color = v_color;
 }
