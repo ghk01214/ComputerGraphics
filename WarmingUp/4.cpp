e95bf7e73bb25afc0ecdd6b1d2fc0f9622cf0914
aida <ghk01214@naver.com>
2022-09-13 01:30:02 +0900

    Warming up No. 6 기능 추가

---
+++ WarmingUp/4.cpp
@@ -0,0 +1,183 @@
+﻿#include "pch.h"
+#include <conio.h>
+
+std::uniform_int_distribution<int32> uid_width{ 0, 590 };
+std::uniform_int_distribution<int32> uid_height{ 0, 390 };
+std::uniform_int_distribution<int32> uid_move{ 10, 100 };
+
+enum
+{
+	rect1 = 0,
+	rect2
+};
+
+struct POINT
+{
+	int32 x, y;
+};
+
+struct RECT
+{
+	POINT lt;
+	POINT rb;
+};
+
+void MoveX(RECT* rect, const int32& direction, const int32& distance)
+{
+	if (rect->lt.x + (direction * distance) < 0)
+	{
+		std::cout << std::format("Cannot move to x direction(out of range)\n");
+		return;
+	}
+
+	if (rect->rb.x + (direction * distance) > 800)
+	{
+		std::cout << std::format("Cannot move to x direction(out of range)\n");
+		return;
+	}
+
+	rect->lt.x += direction * distance;
+	rect->rb.x += direction * distance;
+}
+
+void MoveY(RECT* rect, const int32& direction, const int32& distance)
+{
+	if (rect->lt.y + (direction * distance) < 0)
+	{
+		std::cout << std::format("Cannot move to y direction(out of range)\n");
+		return;
+	}
+
+	if (rect->rb.y + (direction * distance) > 600)
+	{
+		std::cout << std::format("Cannot move to y direction(out of range)\n");
+		return;
+	}
+
+	rect->lt.y += direction * distance;
+	rect->rb.y += direction * distance;
+}
+
+bool CheckCollision(RECT* r1, RECT* r2)
+{
+	if (r2->lt.x <= r1->rb.x && r1->rb.x <= r2->rb.x)
+	{
+		if (r2->lt.y <= r1->rb.y && r1->rb.y <= r2->rb.y)
+			return true;
+		
+		if (r2->lt.y <= r1->lt.y && r1->lt.y <= r2->rb.y)
+			return true;
+	}
+
+	if (r2->lt.x <= r1->lt.x && r1->lt.x <= r2->rb.x)
+	{
+		if (r2->lt.y <= r1->rb.y && r1->rb.y <= r2->rb.y)
+			return true;
+
+		if (r2->lt.y <= r1->lt.y && r1->lt.y <= r2->rb.y)
+			return true;
+	}
+
+	return false;
+}
+
+void run4()
+{
+	std::vector<int32> size;
+	size.reserve(2);
+
+	switch (uid(dre))
+	{
+		case 0:
+		{
+			size.push_back(50);
+			size.push_back((uid(dre) + 1) * 70);
+		}
+		break;
+		case 1:
+		{
+			size.push_back(100);
+			size.push_back((uid(dre) + 1) * 70);
+		}
+		break;
+		case 2:
+		{
+			size.push_back(200);
+			size.push_back((uid(dre) + 1) * 70);
+		}
+		break;
+	}
+
+	POINT p1{ uid_width(dre), uid_height(dre) };
+	POINT p2{ uid_width(dre), uid_height(dre) };
+
+	RECT r1{ { p1.x, p1.y }, { p1.x + size[rect1], p1.y + size[rect1]} };
+	RECT r2{ { p2.x, p2.y }, { p2.x + size[rect2], p2.y + size[rect2]} };
+
+	std::vector<int32> move{ uid_move(dre), uid_move(dre) };
+
+	std::cout << std::format("RECT 1 : ({}, {}), ({}, {})\n", r1.lt.x, r1.lt.y, r1.rb.x, r1.rb.y);
+	std::cout << std::format("RECT 2 : ({}, {}), ({}, {})\n", r2.lt.x, r2.lt.y, r2.rb.x, r2.rb.y);
+	std::cout << std::endl;
+
+	bool flag_collide{ CheckCollision(&r1, &r2) };
+	bool flag_rect1_move{ true };
+
+	while (flag_collide == false)
+	{
+		switch (_getch())
+		{
+			case 'W': FALLTHROUGH
+			case 'w':
+			{
+				if (flag_rect1_move == true)
+					MoveY(&r1, -1, move[rect1]);
+				else
+					MoveY(&r2, -1, move[rect2]);
+			}
+			break;
+			case 'A': FALLTHROUGH
+			case 'a':
+			{
+				if (flag_rect1_move == true)
+					MoveX(&r1, -1, move[rect1]);
+				else
+					MoveX(&r2, -1, move[rect2]);
+			}
+			break;
+			case 'S': FALLTHROUGH
+			case 's':
+			{
+				if (flag_rect1_move == true)
+					MoveY(&r1, 1, move[rect1]);
+				else
+					MoveY(&r2, 1, move[rect2]);
+			}
+			break;
+			case 'D': FALLTHROUGH
+			case 'd':
+			{
+				if (flag_rect1_move == true)
+					MoveX(&r1, 1, move[rect1]);
+				else
+					MoveX(&r2, 1, move[rect2]);
+			}
+			break;
+		}
+
+		if (flag_rect1_move == true)
+			std::cout << std::format("RECT 1 moved\n");
+		else
+			std::cout << std::format("RECT 2 moved\n");
+
+		flag_rect1_move = !flag_rect1_move;
+
+		std::cout << std::format("RECT 1 : ({}, {}), ({}, {})\n", r1.lt.x, r1.lt.y, r1.rb.x, r1.rb.y);
+		std::cout << std::format("RECT 2 : ({}, {}), ({}, {})\n", r2.lt.x, r2.lt.y, r2.rb.x, r2.rb.y);
+		std::cout << std::endl;
+
+		flag_collide = CheckCollision(&r1, &r2);
+	}
+
+	std::cout << std::format("RECT 1 & RECT 2 Collide!!\n");
+}
---
+++ WarmingUp/6.cpp
@@ -0,0 +1,235 @@
+﻿#include "pch.h"
+#include <conio.h>
+
+struct POINT
+{
+	int32 x, y;
+};
+
+enum
+{
+	left = 0,
+	right,
+	up,
+	down
+};
+
+std::uniform_int_distribution<int32> uid_direction{ left, down };
+std::uniform_int_distribution<int32> uid_step{ 1, 8 };
+
+void FindNewDirection(POINT* p, int32* direction, int32 prev_direction)
+{
+	int32 blocked{};
+
+	if (p->x == 0)
+	{
+		if (*direction == left)
+			blocked |= *direction;
+	}
+	else if (p->x == 29)
+	{
+		if (*direction == right)
+			blocked |= *direction;
+	}
+
+	if (p->y == 0)
+	{
+		if (*direction == up)
+			blocked |= *direction;
+	}
+	else if (p->y == 29)
+	{
+		if (*direction == down)
+			blocked |= *direction;
+	}
+
+	while ((blocked & *direction) == *direction || *direction == prev_direction)
+	{
+		*direction = uid_direction(dre);
+	}
+}
+
+int32 MakeRoute(std::vector<std::vector<int32>>* coord)
+{
+	POINT p{ 0, 0 };
+
+	int32 prev_drection{ left };
+	int32 i{ 0 };
+
+	while (p.x != 29 || p.y != 29)
+	{
+		int32 direction{ uid_direction(dre) };
+
+		FindNewDirection(&p, &direction, prev_drection);
+
+		int32 step{ uid_step(dre) };
+
+		switch (direction)
+		{
+			case left:
+			{
+				if (p.x - step < 0)
+					step = p.x;
+
+				for (int32 j = 0; j < step; ++j)
+				{
+					(*coord)[p.y][p.x--] = ++i;
+				}
+			}
+			break;
+			case right:
+			{
+				if (p.x + step > 29)
+					step = 29 - p.x;
+
+				for (int32 j = 0; j < step; ++j)
+				{
+					(*coord)[p.y][p.x++] = ++i;
+				}
+			}
+			break;
+			case up:
+			{
+				if (p.y - step < 0)
+					step = p.y;
+
+				for (int32 j = 0; j < step; ++j)
+				{
+					(*coord)[p.y--][p.x] = ++i;
+				}
+			}
+			break;
+			case down:
+			{
+				if (p.y + step > 29)
+					step = 29 - p.y;
+
+				for (int32 j = 0; j < step; ++j)
+				{
+					(*coord)[p.y++][p.x] = ++i;
+				}
+			}
+			break;
+		}
+
+		prev_drection = direction;
+	}
+
+	(*coord)[p.y][p.x] = ++i;
+
+	return i;
+}
+
+void PrintRoute(std::vector<std::vector<int32>>* coord, int32 steps)
+{
+	for (auto& v : *coord)
+	{
+		for (auto& c : v)
+		{
+			std::cout << std::format(" {}", c);
+		}
+
+		std::cout << std::endl;
+	}
+
+	std::cout << std::format("Total steps : {}", steps);
+}
+
+void MoveRoute(std::vector<std::vector<int32>>* coord, int32 direction)
+{
+	std::vector<int32> temp;
+	temp.reserve(30);
+
+	if (direction == left)
+	{
+		for (int32 i = 0; i < temp.capacity(); ++i)
+		{
+			temp.push_back((*coord)[i][0]);
+		}
+
+		for (int32 i = 0; i < coord->size(); ++i)
+		{
+			for (int32 j = 0; j < (*coord)[i].size() - 1; ++j)
+			{
+				(*coord)[i][j] = (*coord)[i][j + 1];
+			}
+		}
+
+		for (int32 i = 0; i < temp.size(); ++i)
+		{
+			(*coord)[i][29] = temp[i];
+		}
+	}
+	else if (direction == right)
+	{
+		for (int32 i = 0; i < temp.capacity(); ++i)
+		{
+			temp.push_back((*coord)[i][29]);
+		}
+
+		for (int32 i = 0; i < coord->size(); ++i)
+		{
+			for (int32 j = (*coord)[i].size() - 1; j > 0; --j)
+			{
+				(*coord)[i][j] = (*coord)[i][j - 1];
+			}
+		}
+
+		for (int32 i = 0; i < temp.size(); ++i)
+		{
+			(*coord)[i][0] = temp[i];
+		}
+	}
+}
+
+void run6()
+{
+	std::vector<int32> line(30, 0);
+	std::vector<std::vector<int32>> coord(30, line);
+
+	int32 steps{ MakeRoute(&coord) };
+	PrintRoute(&coord, steps);
+
+	while (true)
+	{
+		int c{ _getch() };
+
+		std::system("cls");
+
+		switch (c)
+		{
+			case 'R': FALLTHROUGH
+			case 'r':
+			{
+				for (auto& v : coord)
+				{
+					v = line;
+				}
+
+				steps = MakeRoute(&coord);
+			}
+			break;
+			case 'A': FALLTHROUGH
+			case 'a':
+			{
+				MoveRoute(&coord, left);
+			}
+			break;
+			case 'D': FALLTHROUGH
+			case 'd':
+			{
+				MoveRoute(&coord, right);
+			}
+			break;
+			case 'Q': FALLTHROUGH
+			case 'q':
+			{
+				std::cout << std::format("프로그램 종료\n");
+
+				return;
+			}
+		}
+
+		PrintRoute(&coord, steps);
+	}
+}
